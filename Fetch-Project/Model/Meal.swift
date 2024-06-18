8bc0ff4c990e1f8eaa831561eecf37d5181fbe2c
Siva Ram Reddy Kathi <kathishivaram@gmail.com>
2024-06-18 02:09:36 -0500

    Create Model classes for Meal and MealDetail

---
+++ Fetch-Project/Model/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  Fetch-Project
+//
+//  Created by Shiva on 6/18/24.
+//
+
+import Foundation
+
+struct Meal: Codable {
+    let idMeal: String
+    let strMeal: String
+    let strMealThumb: String?
+}
---
+++ Fetch-Project/Model/MealDetail.swift
@@ -0,0 +1,56 @@
+//
+//  MealDetail.swift
+//  Fetch-Project
+//
+//  Created by Shiva on 6/18/24.
+//
+
+import Foundation
+
+import Foundation
+
+struct MealDetail: Codable {
+    let idMeal: String
+    let strMeal: String
+    let strInstructions: String
+    let strMealThumb: String?
+    let ingredients: [String]
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        idMeal = try container.decode(String.self, forKey: .idMeal)
+        strMeal = try container.decode(String.self, forKey: .strMeal)
+        strInstructions = try container.decode(String.self, forKey: .strInstructions)
+        strMealThumb = try container.decodeIfPresent(String.self, forKey: .strMealThumb)
+        
+        // Decode ingredients dynamically
+        var ingredients: [String] = []
+        let dynamicContainer = try decoder.container(keyedBy: DynamicCodingKeys.self)
+        for key in dynamicContainer.allKeys {
+            if key.stringValue.starts(with: "strIngredient"),
+               let ingredient = try dynamicContainer.decodeIfPresent(String.self, forKey: key),
+               !ingredient.isEmpty {
+                ingredients.append(ingredient)
+            }
+        }
+        self.ingredients = ingredients
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case idMeal, strMeal, strInstructions, strMealThumb
+    }
+    
+    private struct DynamicCodingKeys: CodingKey {
+        var stringValue: String
+        var intValue: Int? { return nil }
+        
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+        
+        init?(intValue: Int) {
+            return nil
+        }
+    }
+}
