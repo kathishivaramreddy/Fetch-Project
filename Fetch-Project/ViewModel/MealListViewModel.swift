648062aa53e3c6c60e78e0045d8ea3bd618f7f34
Siva Ram Reddy Kathi <kathishivaram@gmail.com>
2024-06-18 02:13:10 -0500

    Implement MealListViewModel for fetching meal list

---
+++ Fetch-Project/ViewModel/MealListViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  MealListViewModel.swift
+//  Fetch-Project
+//
+//  Created by Shiva on 6/18/24.
+//
+
+import Foundation
+
+import Foundation
+import Alamofire
+
+class MealListViewModel {
+    var meals: [Meal] = []
+    
+    func fetchMeals(completion: @escaping (Result<[Meal], Error>) -> Void) {
+        let url = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
+        AF.request(url).responseDecodable(of: [String: [Meal]].self) { response in
+            switch response.result {
+            case .success(let value):
+                self.meals = value["meals"]?.sorted(by: { $0.strMeal < $1.strMeal }) ?? []
+                completion(.success(self.meals))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
